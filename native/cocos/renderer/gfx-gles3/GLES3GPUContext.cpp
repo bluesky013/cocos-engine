66cb92e6d480a23a68e048572b329c128e400257
zach <bluesky013@126.com>
2023-03-21 14:30:11 +0800

    headless egl config.

--- native/cocos/renderer/gfx-gles3/GLES3GPUContext.cpp
+++ native/cocos/renderer/gfx-gles3/GLES3GPUContext.cpp
@@ -136,8 +136,8 @@ bool GLES3GPUContext::initialize(GLES3GPUStateCache *stateCache, GLES3GPUConstan
     EGLint sampleSize{msaaEnabled ? EGL_DONT_CARE : 0};
 
     EGLint defaultAttribs[]{
-        EGL_SURFACE_TYPE, EGL_PBUFFER_BIT,
-        EGL_RENDERABLE_TYPE, EGL_OPENGL_ES3_BIT_KHR,
+//        EGL_SURFACE_TYPE, EGL_PBUFFER_BIT,
+//        EGL_RENDERABLE_TYPE, EGL_OPENGL_ES3_BIT_KHR,
         EGL_BLUE_SIZE, blueSize,
         EGL_GREEN_SIZE, greenSize,
         EGL_RED_SIZE, redSize,
